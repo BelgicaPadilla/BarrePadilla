4b797b4aeb0043b192c08ad2ee5b9d66b4803697
BelgicaPadilla <belgica.barre@utelvt.edu.ec>
2022-11-21 15:19:12 -0500

    cambie mi informacion

--- index.php
+++ index.php
@@ -12,12 +12,12 @@ include("marco-right.php");
    <div  style="width: 100%;">     
     <div style="width: 100%; height:100%; margin: auto ; display:inline-block;" >
        <div style=" width:100%; height: 100%; top:0; font-size:20px; text-align:center; float:left;">
-        	<h1>¿Quién es Stalin Francis?</h1>
+        	<h1>¿Quién es Barre Padilla Belgica?</h1>
        		 <hr>
              
           <div id="posteo" style="text-align: left; font-size:0.5vw;">
-                Posteado por:  Stalin Francis<br>
-                Fecha: 2021-08-17 <br>
+                Posteado por:  Barre Padilla Belgica<br>
+                Fecha: 2022-11-20 <br>
           </div>
         </div>
     </div>
@@ -25,8 +25,8 @@ include("marco-right.php");
   <div style="width: 100%; height:100%;" >
            <div  style="text-align:justify; font-size: 20px;">
                    <p class="eys-parrafo">Hola, yo soy Barre Padilla Belgica, Nacida en la ciudad de Esmeraldas, canton Esmeralda, parroquia 5 de Agosto. naci el 13 de Agosto del 1992, estudiante de la carrera de ING. en Tecnologia de la informacion y la Comunicacion en la universidad tecnica Luis Vargas Torres.  </p>
-                   <p class="eys-parrafo">Mis estudios orimatios fueron em la escuela desaparecida Unidad Educativa Bomivar Vodeto Copete, mis estudios secundsrios guen en la unidad educativa Eloy Alfaro, en la cual obtuve el titulo de bachiller en Estudios Sociales en el año 2017. </p>
-<p class="eys-parrafo"> Actualmrnte curso en 7mo semestre de la carrera de ing. en tecnologia de la infirmacion y la comunicacion. <b><a href="https://en.wikipedia.org/wiki/BASIC"> basic</a></b> En la ciudad de Esmeraldas.</p>
+                   <p class="eys-parrafo">Mis estudios primarios fueron em la escuela desaparecida Unidad Educativa Bolivar Vodero Copete, mis estudios secundarios fueron en la unidad educativa Eloy Alfaro, en la cual obtuve el titulo de bachiller en Estudios Sociales en el año 2017. </p>
+<p class="eys-parrafo"> Actualmente curso en 7mo semestre de la carrera de ing. en tecnologia de la informacion y la comunicacion. <b><a href="https://en.wikipedia.org/wiki/BASIC"> basic</a></b> En la ciudad de Esmeraldas.</p>
 
    <div id="blog-seccion2"  style="height: 50px;"></div>
 
@@ -34,7 +34,7 @@ include("marco-right.php");
     
 
 
-                   <p class="eys-parrafo">  en medio de ka carrera he realizado drminarios<p class="eys-parrafo">Me he desempeñado en diferntes areas laborables, como es cyber, atencion al cliente, comerciante</p>
+                   <p class="eys-parrafo">  en medio de la carrera he realizado seminarios<p class="eys-parrafo">Me he desempeñado en diferntes areas laborables, como es cyber, atencion al cliente, comerciante</p>
 <p class="eys-parrafo"> Por las exigencias de la universidad, dividi mi horario entre el trabajo y la universidad.</p>
    <div id="blog-seccion3"  style="height: 50px;"></div>
 
@@ -42,14 +42,14 @@ include("marco-right.php");
     
 
 
-                   <p  class="eys-parrafo">Mi perfil tecnológico es la programación de computadoras, me eh espevializado en java scrit, c++</p> 
-                   <p  class="eys-parrafo">Hoy en día me sigobpreparaando para set mejor profesional.</p>
+                   <p  class="eys-parrafo">Mi perfil tecnológico es la programación de computadoras, me eh especializado en java scrit, c++</p> 
+                   <p  class="eys-parrafo">Hoy en día me sigo preparaando para ser  mejor profesional.</p>
    <div id="blog-seccion4"  style="height: 50px;"></div>
 
-    <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
+    <h2 style="border-bottom: 1px solid green;"> Mis Hobby</h2> 
     
 
-                   <p   class="eys-parrafo">Me gusta leer mucho, viajar e instruirme y adquirir conocimiento referente a mi carrera, mantrnetme al dia en todo lo que es referente en la tecnologia.</p>
+                   <p   class="eys-parrafo">Me gusta leer mucho, viajar e instruirme y adquirir conocimiento referente a mi carrera, mantenerme al dia en todo lo que es referente en la tecnologia.</p>
        
 
                    
@@ -79,9 +79,9 @@ include("marco-left.php")
  
 <script >  
 
- document.title="Stalin"; 
+ document.title="Barre Padilla Belgica"; 
 document.getElementById("foto").src="./images/stalin.png";
-document.getElementById("elnombre").innerHTML="Stalin Francis";
+document.getElementById("elnombre").innerHTML="Barre Padilla Belgica";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
 
